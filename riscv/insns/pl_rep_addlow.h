d2cc56381d619fd7f8f34a0f4f207b0c18c79ad1
plasx0n <your@email.com>
2024-04-01 03:51:17 +0200

    polar

--- riscv/insns/pl_rep_addlow.h
+++ riscv/insns/pl_rep_addlow.h
@@ -1,1 +1,23 @@
-//need to fill this one out 
+
+int a   = RS1 ; 
+int b   = RS2 ; 
+
+int res; 
+int16_t r1,r2 ; 
+int16_t a1,a2 ; 
+int8_t b1,b2 ; 
+
+
+a1 = (a<<16 )>>16 ; 
+a2 = (a     )>>16 ; 
+
+b1 = (b<<24)>>24;
+b2 = (b<<16)>>24;
+
+r1 = a1+b1 ; 
+r2 = a2+b2 ;
+
+// res =  (r1 & 0xFFFF) | ((r2<<16)&0xFFFF0000)
+res = (r1 & 0xFFFF) |( (r2<<16)&0xFFFF0000 );  
+
+WRITE_RD(res) ; 
--- riscv/insns/pl_rep_addsum.h
+++ riscv/insns/pl_rep_addsum.h
@@ -1,2 +1,23 @@
 //need to fill this one out 
 
+int a   = RS1 ; 
+int b   = RS2 ; 
+
+int16_t a1,a2 ; 
+int16_t b1,b2 ; 
+
+
+a1 = (a<<16 )>>16 ; 
+a2 = (a     )>>16 ; 
+
+b1 = (b<<16 )>>16;
+b2 = (b     )>>16;
+
+a1 = (a1<0)?0x01:0X00 ;  
+a2 = (a2<0)?0x01:0X00 ;  
+b1 = (b1<0)?0x01:0X00 ;  
+b2 = (b2<0)?0x01:0X00 ;  
+
+int res = (a1 & 0xFF) | ((a2<<8)&0xFF00)  | ((b1<<16)&0xFF0000) | ((b2<<24)&0xFF000000);
+
+WRITE_RD(res) ; 
